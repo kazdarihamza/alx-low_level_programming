946db1870be95856b3f777f1f1e7937095b79493
Hamza Kazdari <kazdari.hamza@gmail.com>
2023-03-16 18:22:13 +0100

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+# include <stdio.h>
+/**
+ * main - main function
+ * Return: it should always be 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	Return(0);
+}
