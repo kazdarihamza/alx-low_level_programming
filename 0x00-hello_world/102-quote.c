1172f787206c908272bc34f40a5c530b9bbca459
Hamza Kazdari <kazdari.hamza@gmail.com>
2023-03-16 18:57:18 +0100

    quote

---
+++ 0x00-hello_world/102-quote.c
@@ -0,0 +1,13 @@
+# include<stdio.h>
+# include<unistd.h>
+
+/**
+ * main - entry point
+ * Return: 1 in this case means failure
+ */
+int main(void)
+{
+	write(2,
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
+	return (1);
+}
