207ebd179805b9f6d1f17a10dd982d2880c9570d
Hamza Kazdari <kazdari.hamza@gmail.com>
2023-03-16 18:16:30 +0100

    puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
-*main - this is a description for main
-*return: return should always be 0
-*/
+ * main - this is a description for main
+ * Return: return should always be 0
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
