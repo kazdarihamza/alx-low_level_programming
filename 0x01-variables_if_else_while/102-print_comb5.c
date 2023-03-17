f9beb6b8ecc33ad5a7c6fe2ccf57cbe4e75fdb46
Hamza Kazdari <kazdari.hamza@gmail.com>
2023-03-17 21:51:42 +0100

    print comb 5

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	int n, m;
+
+	for (n = 0; n <= 9; n++)
+	{
+		for (m = 0; m <= 9; m++)
+		{
+			putchar(n + '0');
+			putchar(m + '0');
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
