0ffe0ef6e53ebd879b7a262e411e478863f02b54
Hamza Kazdari <kazdari.hamza@gmail.com>
2023-03-23 14:30:48 +0000

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - function that prints numbers from 0 to 9
+ */
+
+void print_numbers(void)
+{
+	int i = 48;
+
+	while (i <= 57)
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+}
