af4c77dd677a664cd36e881c054a4d38ff40899d
Hamza Kazdari <kazdari.hamza@gmail.com>
2023-03-23 14:12:26 +0000

    multiplicate

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * mul - function that multiplicate 2 integers
+ * @a: first integer
+ * @b: second integer
+ * Return: always 0 (success)
+ */
+
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+	_putchar(result);
+	return (0);
+}
