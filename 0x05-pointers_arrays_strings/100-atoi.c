f0892afb6b87e6be03acb67d615608a1b6364eee
Hamza Kazdari <kazdari.hamza@gmail.com>
2023-03-27 22:28:07 +0000

    task 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,30 +1,41 @@
+#include "main.h"
+
 /**
- * _atoi - convert a string to an integer
- * @s: the string to convert
+ * _atoi - convert a string to an integer.
+ * @s: pointer to string
  *
- * Return: the integer value of the string, or 0 if no number is found
+ * Return: integer value of string, or 0 if no digits are found
  */
 int _atoi(char *s)
 {
-	int sign = 1;
-	int result = 0;
-	int i = 0;
+    int sign = 1;
+    int result = 0;
+    int digit_found = 0;
 
-	if (s[0] == '-')
-	{
-		sign = -1;
-		i++;
-	}
-	for (; s[i] != '\0'; i++)
-	{
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			result = result * 10 + (s[i] - '0');
-		}
-		else
-		{
-			break;
-		}
-	}
-	return (sign * result);
+    while (*s)
+    {
+        if (*s == '-')
+            sign *= -1;
+
+        if (*s >= '0' && *s <= '9')
+        {
+            digit_found = 1;
+            result = result * 10 + (*s - '0');
+
+            /* Check for overflow */
+            if (result < 0)
+            {
+                if (sign == 1)
+                    return 2147483647;
+                else
+                    return -2147483648;
+            }
+        }
+        else if (digit_found)
+            break;
+
+        s++;
+    }
+
+    return (sign * result);
 }
