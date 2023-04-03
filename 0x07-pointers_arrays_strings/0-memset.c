e4061cd651647c7f70d4b04eaffe92640042e985
Hamza Kazdari <kazdari.hamza@gmail.com>
2023-04-03 12:35:12 +0000

    task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -8,11 +8,10 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i = 0;
+	unsigned int i;
 
-	while (i < n - 1)
+	for (i = 0; n > 0; i++, n--)
 	{
-		i++;
 		s[i] = b;
 	}
 	return (s);
