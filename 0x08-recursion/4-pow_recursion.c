781b3d1d6b47a890b8278cd2f0ffc6b6fad0181a
Hamza Kazdari <kazdari.hamza@gmail.com>
2023-04-05 15:25:23 +0000

    task 4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -7,16 +7,12 @@
  */
 int _pow_recursion(int x, int y)
 {
+	if (y < 0)
+		return (-1);
 	if (x == 0)
 		return (0);
 	if (y == 0)
 		return (1);
-	else if (y == 1)
-		return (x);
-	if (y > 1)
-	{
-		return (x * _pow_recursion(x, y - 1));
-	}
-	else if (y < 0)
-		return (-1);
+		
+	return (x * _pow_recursion(x, y - 1));
 }
