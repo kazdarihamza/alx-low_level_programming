77f43c02b6d285008b391c77ec716099a919f589
Hamza Kazdari <kazdari.hamza@gmail.com>
2023-04-05 14:40:29 +0000

    task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _strlen_recursion - prints the length of a string
+ * @s: string
+ * Return: string length
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	s++;
+	return (_strlen_recursion(s) + 1);
+}
