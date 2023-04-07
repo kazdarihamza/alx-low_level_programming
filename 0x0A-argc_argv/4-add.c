fa67be44584858c42e5d2e1e83df6cfeb68998a6
Hamza Kazdari <kazdari.hamza@gmail.com>
2023-04-07 21:06:09 +0000

    task 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * main - prints the number of arguments passed into it.
+ * @argc: argument count
+ * @argv: argument variable
+ * Return: always 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	int sum = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		char *arg = argv[i];
+		int j;
+
+		for (j = 0; arg[j] != '\0'; j++)
+		{
+			if (arg[j] < '0' || arg[j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
