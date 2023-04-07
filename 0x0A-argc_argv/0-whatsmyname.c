3423c6fe6cb8821af4368509ff18a483324f757d
Hamza Kazdari <kazdari.hamza@gmail.com>
2023-04-07 19:49:29 +0000

    task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- *
+ * main - prints the name of the file
+ * @argc: arg count
+ * @argv: arg variable
+ * Return: alywas 0 (success)
  */
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 	if (argc <= 1)
 	{
