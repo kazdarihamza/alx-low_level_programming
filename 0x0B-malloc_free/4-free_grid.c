3dab5d939b7c49c256d698decb14017d33ccb643
Hamza Kazdari <kazdari.hamza@gmail.com>
2023-04-11 20:01:24 +0000

    task 4

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -6,16 +6,16 @@
  *@height: height of grid
  *@grid: pointer to a grid
  */
- void free_grid(int **grid, int height)
- {
- 	int i;
- 
- 	if (grid == NULL || grid == 0)
- 		return;
- 
- 	for (i = 0; i < height; i++)
- 	{
- 		free(grid[i]);
- 	}
- 	free(grid);
- }
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	if (grid == NULL || grid == 0)
+		return;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+}
