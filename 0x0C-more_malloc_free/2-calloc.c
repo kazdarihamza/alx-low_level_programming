da8032690367e7bb45b6422dc60e3f95bde5186f
Hamza Kazdari <kazdari.hamza@gmail.com>
2023-04-13 18:28:31 +0000

    task 2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,25 @@
+#include "notrebloh.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * *_calloc - function to allocates memory
+ * @nmemb: unsigned int type
+ * @size: unsigned int type
+ * Return: return pointer to array
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *str;
+	unsigned int n;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	str = malloc(nmemb * size);
+	if (str == NULL)
+		return (NULL);
+	for (n = 0; n < size * nmemb; n++)
+		str[n] = 0;
+	return (str);
+}
