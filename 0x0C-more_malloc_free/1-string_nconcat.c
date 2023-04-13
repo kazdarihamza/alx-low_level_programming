b80508475ab13509a2816c0c423ac8ceb9be1332
Hamza Kazdari <kazdari.hamza@gmail.com>
2023-04-13 18:09:02 +0000

    task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,8 +13,7 @@ int _strlen(char *s)
 	int i;
 
 	for (i = 0; s[i] != '\0'; i++)
-	{
-	}
+		;
 	return (i);
 }
 /**
@@ -34,6 +33,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
+	if (len < 0)
+		return(NULL);
+
 	if (len >= len2)
 	{
 		len = len2;
@@ -43,13 +45,14 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		str = malloc(sizeof(char) * (len1 + len + 1));
 	if (str == NULL)
 		return (NULL);
+
 	for (i = 0; i < len1; i++)
 	{
 		str[i] = s1[i];
 	}
 	for (j = 0; j < len + 1; j++)
 	{
-		str[i++] = s2[j];
+		str[i + j] = s2[j];
 	}
 	return (str);
 }
