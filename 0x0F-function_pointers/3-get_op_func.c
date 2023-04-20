a623c64a23ed8fd8620f22d3b6ad7fc03749d1e8
Hamza Kazdari <kazdari.hamza@gmail.com>
2023-04-20 00:58:58 +0000

    task 3

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -22,10 +22,11 @@ int (*get_op_func(char *s))(int, int)
 	};
 	int i = 0;
 
-	while (ops[i].op != NULL && *(ops[i].op) != *s)
+	while (ops[i].op != NULL)
 	{
+		if (*s == *ops[i].op)
+			return (ops[i].f);
 		i++;
-		return (ops[i].f);
 	}
 	return (NULL);
 }
