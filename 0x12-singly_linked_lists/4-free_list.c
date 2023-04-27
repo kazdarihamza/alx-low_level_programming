3b87dafed1532089b15fdc04f5973e85a1234330
Hamza Kazdari <kazdari.hamza@gmail.com>
2023-04-27 22:26:46 +0100

    task 4

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,13 +1,23 @@
 #include "lists.h"
 
 /**
-*
+*free_list - frees a list
+*@head: pointer to the first node
+*Return: void
 */
 
 void free_list(list_t *head)
 {
+	list_t *tmp;
+	int i;
 
-
-
+	
+	for (i = 0; head != NULL; i++)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp->str);
+		free(tmp);
+	}
 }
 
