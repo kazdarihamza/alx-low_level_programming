6a365f7cefc6bfc54ecbeeec39f0e6d9d69fbd40
Hamza Kazdari <kazdari.hamza@gmail.com>
2023-04-27 17:29:09 +0100

    task 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,13 +1,33 @@
 #include "lists.h"
 
 /**
-*
+*print_list - adds a new noe at the beginnigng of a list
+*@head: pointer to beginning of a list
+*@str: value of the node to add
+@Return: the address of the new element
 */
 
-size_t print_list(const list_t *h)
+list_t *add_node(list_t **head, const char *str)
 {
+	list_t *new;
+	int len;
+	char *newstr;
 
+	new = malloc(sizeof(list_t));
+	if (new == 0)
+		return (0);
+	newstr = strdup(str);
+	if (!newstr)
+	{
+		free(new);
+		return (0);
+	}
 
+	new->str = newstr;
+	new->len = strlen(str);
+	new->next = *head; 
 
+	*head = new;
+
+	return (new);
 }
-
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
 
 
 /**
@@ -25,5 +26,6 @@ typedef struct list_s
 int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
 
 #endif
