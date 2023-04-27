72a6908f35448e989c9fa56e6293e01d142f043a
Hamza Kazdari <kazdari.hamza@gmail.com>
2023-04-27 22:42:07 +0100

    task 5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+*premain - premain entry
+*/
+
+void __attribute__((constructor)) premain()
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
