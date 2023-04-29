886ae76ef3114724299aff0771b17515d156ae22
Hamza Kazdari <kazdari.hamza@gmail.com>
2023-04-29 21:31:30 +0100

    task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+*free_list - frees a list
+*@head: pointer to the first node
+*/
+
+void free_listint2(listint_t **head)
+{
+	listint_t *placement;
+	listint_t **temp = head;
+
+	if (temp != NULL)
+	{
+		while (head != NULL)
+		{
+			placement = *head;
+			free(placement);
+			*head = (*head)->next;
+		}
+	*temp = NULL;
+}
