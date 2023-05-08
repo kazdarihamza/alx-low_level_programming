5896b0d73295ca04d73f2b981fb9e886b18d4f30
Hamza Kazdari <kazdari.hamza@gmail.com>
2023-05-08 21:37:55 +0100

    task 2

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ * _strlen - a function that appends text at the end of a file.
+ * @str: string
+ * Return: length
+ */
+int _strlen(char *str)
+{
+	int len;
+
+	for (len = 0; str[len] != '\0'; len++)
+		;
+
+	return (len);
+}
+
+/**
+* create_file - creates file with permissions rw------- and writes content in
+* if file already exists, don't change permissions and just truncate it
+* @filename: name to give to new file
+* @text_content: writes this content into file
+* Return: 1 on success, -1 on error
+*/
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, w;
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	if (!text_content)
+	{
+		close(fd);
+		return (1);
+	}
+
+	w = write(fd, text_content, _strlen(text_content));
+	if (w == -1 || w != _strlen(text_content))
+	{
+		close(fd);
+		return (-1);
+	}
+
+	close (fd);
+	return (1);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,7 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -10,5 +9,6 @@
 #include <fcntl.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
 
 #endif
