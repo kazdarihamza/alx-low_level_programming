eeaa652a96d0a031589ae5e9275f000c2c5eef88
Hamza Kazdari <kazdari.hamza@gmail.com>
2023-06-15 21:29:51 +0100

    task 8

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -27,6 +27,17 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		free(node);
 		return (1);
 	}
+	for (count = 0; node != NULL && count < index - 1 ; count++)
+		node = node->next;
+	if (node == NULL || node->next == NULL)
+		return (-1);
+	if (node->next->next != NULL)
+	{
+		node->next = node->next->next;
+		free(node->next->prev);
+		node->next->prev = node;
+		return (1);
+	}
 	else
 	{
 		free(node->next);
